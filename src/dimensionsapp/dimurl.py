25b5b9d30835a728bcb47a06d8750f9d72c69275
gonchigor <gonchigor@tut.by>
2019-04-19 00:41:25 +0300

    done listview

--- src/dimensionsapp/dimurl.py
+++ src/dimensionsapp/dimurl.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from dimensionsapp.views import AuthorDetailView, SerieDetailView, JenreDetailView, PublishingHouseDetailView, \
-     FormatBookDetailView, BindingDetailView, AgeRestrictionDetailView
+     FormatBookDetailView, BindingDetailView, AgeRestrictionDetailView, SerieListView, AuthorListView, \
+     JenreListView, PublishingHouseListView, FormatBookListView, BindingListView, AgeRestrictionListView
 
 urlpatterns = [
-    path('author/<int:pk>/', AuthorDetailView.as_view()),
-    path('serie/<int:pk>/', SerieDetailView.as_view()),
-    path('jenre/<int:pk>/', JenreDetailView.as_view()),
-    path('publishing/<int:pk>/', PublishingHouseDetailView.as_view()),
-    path('format/<int:pk>/', FormatBookDetailView.as_view()),
-    path('binding/<int:pk>/', BindingDetailView.as_view()),
-    path('agerestriction/<int:pk>/', AgeRestrictionDetailView.as_view()),
-    path('serie/', SerieListView.as_view()),
+    path('author/<int:pk>/', AuthorDetailView.as_view(), name='author_detail'),
+    path('serie/<int:pk>/', SerieDetailView.as_view(), name='serie_detail'),
+    path('jenre/<int:pk>/', JenreDetailView.as_view(), name='jenre_detail'),
+    path('publishing/<int:pk>/', PublishingHouseDetailView.as_view(), name='publishing_house_detail'),
+    path('format/<int:pk>/', FormatBookDetailView.as_view(), name='format_book_detail'),
+    path('binding/<int:pk>/', BindingDetailView.as_view(), name='binding_detail'),
+    path('agerestriction/<int:pk>/', AgeRestrictionDetailView.as_view(), name='age_restriction_detail'),
+    path('author/', AuthorListView.as_view(), name='author_list'),
+    path('serie/', SerieListView.as_view(), name='serie_list'),
+    path('jenre/', JenreListView.as_view(), name='jenre_list'),
+    path('publishing/', PublishingHouseListView.as_view(), name='publishing_house_list'),
+    path('format/', FormatBookListView.as_view(), name='format_book_list'),
+    path('binding/', BindingListView.as_view(), name='binding_list'),
+    path('agerestriction/', AgeRestrictionListView.as_view, name='age_restriction_list'),
 ]
--- src/dimensionsapp/views.py
+++ src/dimensionsapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from dimensionsapp.models import Author, Serie, Jenre, PublishingHouse, FormatBook, Binding, AgeRestriction
@@ -35,3 +35,57 @@ class AgeRestrictionDetailView(DetailView):
 
 class SerieListView(ListView):
     model = Serie
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url_detail'] = 'serie_detail'
+        return context
+
+
+class JenreListView(ListView):
+    model = Jenre
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url_detail'] = 'jenre_detail'
+        return context
+
+
+class PublishingHouseListView(ListView):
+    model = PublishingHouse
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url_detail'] = 'publishing_house_detail'
+        return context
+
+
+class FormatBookListView(ListView):
+    model = FormatBook
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url_detail'] = 'format_book_detail'
+        return context
+
+
+class BindingListView(ListView):
+    model = Binding
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url_detail'] = 'binding_detail'
+        return context
+
+
+class AgeRestrictionListView(ListView):
+    model = AgeRestriction
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url_detail'] = 'age_restriction_detail'
+        return context
+
+
+class AuthorListView(ListView):
+    model = Author
